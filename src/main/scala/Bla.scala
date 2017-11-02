45a8ec75de6563dcac9df5781a7f9e253362f573
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-11-02 13:44:54 +0100

    added random README.md writer

--- src/main/scala/Bla.scala
+++ src/main/scala/Bla.scala
@@ -1,3 +1,38 @@
-class Bla {
+import java.io._
+object Bla {
+  def main(): Unit = {
+    generateTextFile(documentation,fileName)
+  }
 
+  def generateTextFile(
+                      text: String,
+                      fileName: String
+                      ): Unit = {
+    val printWriter = new PrintWriter(new File(fileName))
+    printWriter.write(text)
+    printWriter.close()
+  }
+  val documentation: String =
+    """This is a very long string.
+      |It contains newlines.
+      |It is Markdown.
+      |
+      |# YES MARKDOWN
+      |
+      |Who would have thought that?
+      |It also contains
+      |```
+      |arbitrary code
+      |```
+      |And some more `stuff` like that. Such as:
+      |* This
+      |* Stuff
+      |* ReST is better
+      |
+      |Paragraph1
+      |
+      |Paragraph2.
+    """.stripMargin
+
+  val fileName = "README.md"
 }
