a035b72af0c8fcd190c12bfb00e9e711e486bfd1
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-11-02 16:31:01 +0100

    Revert "some unfinished restructuring"
    This reverts commit ab2f76eb607076b743854f96744aced8119f1127.

--- src/main/scala/Bla.scala
+++ src/main/scala/Bla.scala
@@ -4,8 +4,6 @@ object Bla {
     generateTextFile(documentation,fileName)
   }
 
-
-
   def generateTextFile(
                       text: String,
                       fileName: String
@@ -14,38 +12,27 @@ object Bla {
     printWriter.write(text)
     printWriter.close()
   }
+  val documentation: String =
+    """This is a very long string.
+      |It contains newlines.
+      |It is Markdown.
+      |
+      |# YES MARKDOWN
+      |
+      |Who would have thought that?
+      |It also contains
+      |```
+      |arbitrary code
+      |```
+      |And some more `stuff` like that. Such as:
+      |* This
+      |* Stuff
+      |* ReST is better
+      |
+      |Paragraph1
+      |
+      |Paragraph2.
+    """.stripMargin
 
-  abstract class DocEntry(documentation,) {
-    def documentation: String
-
-    def filename: String
-  }
-  class DocEntry {
-    val index: DocEntry = new DocEntry()
-  }
-      """This is a very long string.
-        |It contains newlines.
-        |It is Markdown.
-        |
-        |# YES MARKDOWN
-        |
-        |Who would have thought that?
-        |It also contains
-        |```
-        |arbitrary code
-        |```
-        |And some more `stuff` like that. Such as:
-        |* This
-        |* Stuff
-        |* ReST is better
-        |
-        |Paragraph1
-        |
-        |Paragraph2.
-      """.stripMargin
-
-    val fileName = "docs/INDEX.MD"
-  }
-  }
-
-
+  val fileName = "docs/INDEX.MD"
+}
